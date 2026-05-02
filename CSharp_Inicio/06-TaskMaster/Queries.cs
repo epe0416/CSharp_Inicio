710ffce7f89d5be476ef22d920c64ee675416fb0
Everth Paredes <epe0413@gmail.com>
2026-05-02 06:18:33 -0600

    Sección 7 - Lección: Configuración del Proyecto

---
+++ CSharp_Inicio/06-TaskMaster/Queries.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskMaster
+{
+    public class Queries(List<Task> _tasks)
+    {
+        private List<Task> Tasks = _tasks;
+
+        // Desarrollar métodos para gestionar las tareas 
+
+    }
+}
--- CSharp_Inicio/Program.cs
+++ CSharp_Inicio/Program.cs
@@ -17,6 +17,7 @@ partial class Program
         //DirectoryExample();
         //PathExample();
         //WriteFileExample();
-        ManageJsonFile.Program.ManageJsonFile();
+        //ManageJsonFile.Program.ManageJsonFile();
+        TaskMaster();
     }
 }
